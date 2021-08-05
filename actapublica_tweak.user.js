7edff55d216d2316fa7b228eb5d2845db42b9712
rasasak <adamkrivanek@gmail.com>
2021-08-05 21:43:44 +0200

    Update actapublica_tweak.user.js

--- actapublica_tweak.user.js
+++ actapublica_tweak.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Actapublica tweak
-// @version      0.2
+// @version      0.3
 // @downloadURL  https://raw.githubusercontent.com/rasasak/MZA_tweak/main/actapublica_tweak.user.js
 // @updateURL    https://raw.githubusercontent.com/rasasak/MZA_tweak/main/actapublica_tweak.user.js
 // @description  Malá vylepšení pro web MZA...
@@ -17,11 +17,23 @@ $(document).ready(function() {
    var married = $('#matrikaHeader .row div:nth-child(4) p strong').text()
    var died = $('#matrikaHeader .row div:nth-child(5) p strong').text()
 
-  $('.card-header .nav').append('<li class="nav-item pl-5 pr-3"><span class="small font-italic">Narození od-do</span><br><span class="font-weight-bolder">'+birth+'</span></li>')
-  $('.card-header .nav').append('<li class="nav-item px-3"><span class="small font-italic">Oddaní od-do</span><br><span class="font-weight-bolder">'+married+'</span></li>')
-  $('.card-header .nav').append('<li class="nav-item px-3"><span class="small font-italic">Zemřelí od-do</span><br><span class="font-weight-bolder">'+died+'</span></li>')
-      
-    
+  $('.card-header .nav').append(`<li class="nav-item pl-5">
+                                   <ul class="nav">
+                                     <li class="nav-item px-3">
+                                       <span class="small font-italic">Narození od-do</span><br>
+                                       <span class="font-weight-bolder">`+birth+`</span>
+                                     </li>
+                                     <li class="nav-item px-3">
+                                       <span class="small font-italic">Oddaní od-do</span><br>
+                                       <span class="font-weight-bolder">`+married+`</span>
+                                     </li>
+                                     <li class="nav-item px-3">
+                                       <span class="small font-italic">Zemřelí od-do</span><br>
+                                       <span class="font-weight-bolder">`+died+`</span>
+                                     </li>
+                                   </ul>
+                                 </li>`)
+
     //delete minimap
     $("[id^='navigator-']").remove()
 });
