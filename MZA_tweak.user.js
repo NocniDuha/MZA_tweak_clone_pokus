c3f94c2bdbb2356600c8048bac3d2dc45bed66a4
rasasak <adamkrivanek@gmail.com>
2021-08-12 17:33:38 +0200

    Update MZA_tweak.user.js

--- MZA_tweak.user.js
+++ MZA_tweak.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MZA tweak
-// @version      0.5.5
+// @version      0.6.0
 // @downloadURL  https://github.com/rasasak/MZA_tweak/raw/main/MZA_tweak.user.js
 // @updateURL    https://github.com/rasasak/MZA_tweak/raw/main/MZA_tweak.user.js
 // @description  Malá vylepšení pro web MZA...
@@ -43,6 +43,14 @@ $(document).ready(function() {
     $('#seadragon-toolbar .form-group').after(`<a onclick="dezoomify()" id="download" type="button" class="btn btn-light mr-1" title="Stáhnout (Dezoomify)" style="display: inline-block; position: relative;">
                                                  <i class="fas fa-cloud-download-alt"></i>
                                                </a>`)
+    //preserve button
+    $('#seadragon-toolbar .form-group').after(`<a onclick="preserve()" id="preserve" type="button" class="btn btn-light mr-1" title="Zachovat Zoom" style="display: inline-block; position: relative;"></a>`)
+
+    if (g_viewer.preserveViewport == false){
+        $('#preserve').append(`<i id=preserve_icon class="fas fa-lock-open"></i>`)
+    }else{
+        $('#preserve').append(`<i id=preserve_icon class="fas fa-lock"></i>`)
+    }
 
 
     // normalize
@@ -98,3 +106,17 @@ function dezoomify(){
     window.open(dezoomify_url, '_blank');
 }
 
+if(!unsafeWindow.preserve)
+{
+    unsafeWindow.preserve = preserve;
+}
+
+function preserve(){
+    if (g_viewer.preserveViewport == false){
+        g_viewer.preserveViewport = true
+        $('#preserve_icon').attr('class', 'fas fa-lock')
+    }else{
+        g_viewer.preserveViewport = false
+        $('#preserve_icon').attr('class', 'fas fa-lock-open')
+    }
+}
