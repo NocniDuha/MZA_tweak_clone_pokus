ebe1a52ad516635a9cdc40b2a35b74fa635910ec
rasasak <adamkrivanek@gmail.com>
2021-08-13 11:49:27 +0200

    Update MZA_tweak.user.js

--- MZA_tweak.user.js
+++ MZA_tweak.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MZA tweak
-// @version      0.6.4.2
+// @version      0.6.4.3
 // @downloadURL  https://github.com/rasasak/MZA_tweak/raw/main/MZA_tweak.user.js
 // @updateURL    https://github.com/rasasak/MZA_tweak/raw/main/MZA_tweak.user.js
 // @description  Malá vylepšení pro web MZA...
@@ -43,7 +43,7 @@ $(document).ready(function() {
                                                  <i class="fas fa-cloud-download-alt"></i>
                                                </a>`)
     //preserve button
-    $('#seadragon-toolbar .form-group').after(`<a onclick="preserve()" id="preserve" type="button" class="btn btn-light mr-1" title="Zachovat Zoom" style="display: inline-block; position: relative;"></a>`)
+    $('#seadragon-toolbar .form-group').after(`<a onclick="preserve()" id="preserve" type="button" class="btn btn-light mr-1" title="Zachovat zoom a polohu" style="display: inline-block; position: relative;"></a>`)
 
     if (g_viewer.preserveViewport == false){
         $('#preserve').append(`<i id=preserve_icon class="fas fa-fw fa-lock-open"></i>`)
