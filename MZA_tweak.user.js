1579104f78d5849ea680b49524af893519540ffa
rasasak <adamkrivanek@gmail.com>
2021-08-13 11:16:18 +0200

    Update MZA_tweak.user.js

--- MZA_tweak.user.js
+++ MZA_tweak.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MZA tweak
-// @version      0.6.3
+// @version      0.6.3b
 // @downloadURL  https://github.com/rasasak/MZA_tweak/raw/main/MZA_tweak.user.js
 // @updateURL    https://github.com/rasasak/MZA_tweak/raw/main/MZA_tweak.user.js
 // @description  Malá vylepšení pro web MZA...
@@ -47,9 +47,9 @@ $(document).ready(function() {
     $('#seadragon-toolbar .form-group').after(`<a onclick="preserve()" id="preserve" type="button" class="btn btn-light mr-1" title="Zachovat Zoom" style="display: inline-block; position: relative;"></a>`)
 
     if (g_viewer.preserveViewport == false){
-        $('#preserve').append(`<i id=preserve_icon class="fas fa-lock-open"></i>`)
+        $('#preserve').append(`<i id=preserve_icon class="fas fa-fw fa-lock-open"></i>`)
     }else{
-        $('#preserve').append(`<i id=preserve_icon class="fas fa-lock"></i>`)
+        $('#preserve').append(`<i id=preserve_icon class="fas fa-fw fa-lock"></i>`)
     }
 
 
@@ -104,9 +104,9 @@ if(!unsafeWindow.preserve)
 function preserve(){
     if (g_viewer.preserveViewport == false){
         g_viewer.preserveViewport = true
-        $('#preserve_icon').attr('class', 'fas fa-lock')
+        $('#preserve_icon').attr('class', 'fas fa-fw fa-lock')
     }else{
         g_viewer.preserveViewport = false
-        $('#preserve_icon').attr('class', 'fas fa-lock-open')
+        $('#preserve_icon').attr('class', 'fas fa-fw fa-lock-open')
     }
 }
